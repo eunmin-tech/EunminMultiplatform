3afbb346b311fcbe2685d6cfe190e479a9f783af
eunmin <eunmin@blocksquare.tech>
2025-03-27 18:42:39 +0900

    chore: Work in progress on iOS support

---
+++ pokedex/composeApp/src/iosMain/kotlin/io/eunmin/multiplatform/pokedex/composeApp/MainViewController.kt
@@ -0,0 +1,6 @@
+package io.eunmin.multiplatform.pokedex.composeApp
+
+import androidx.compose.ui.window.ComposeUIViewController
+import io.eunmin.multiplatform.pokedex.navigation.PokedexApp
+
+fun MainViewController() = ComposeUIViewController { PokedexApp() }
