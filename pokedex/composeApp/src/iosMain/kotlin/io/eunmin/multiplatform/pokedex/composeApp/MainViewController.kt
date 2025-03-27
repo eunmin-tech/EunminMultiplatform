5fe61aff29510277cdd1c136623b320b157d425d
eunmin <>
2025-03-27 23:49:10 +0900

    feat: Complete iOS support

--- pokedex/composeApp/src/iosMain/kotlin/io/eunmin/multiplatform/pokedex/composeApp/MainViewController.kt
+++ pokedex/composeApp/src/iosMain/kotlin/io/eunmin/multiplatform/pokedex/composeApp/MainViewController.kt
@@ -3,4 +3,6 @@ package io.eunmin.multiplatform.pokedex.composeApp
 import androidx.compose.ui.window.ComposeUIViewController
 import io.eunmin.multiplatform.pokedex.navigation.PokedexApp
 
-fun MainViewController() = ComposeUIViewController { PokedexApp() }
+fun MainViewController() = ComposeUIViewController(
+    configure = { enforceStrictPlistSanityCheck = false }
+) { PokedexApp() }
